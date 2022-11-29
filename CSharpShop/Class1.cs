bf007267e43bb621c3357f9fe7f1275e098cc26e
Redux234 <lorenzo.gaetano2002@gmail.com>
2022-11-29 14:37:08 +0100

    Add project files.

---
+++ CSharpShop/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product
+{
+    public class Prodotto
+    {
+        public int codice;
+        public string nome;
+        public string descrizione;
+        public float prezzo;
+        public int iva;
+    }
+}
